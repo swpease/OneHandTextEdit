05fa1b303d901f739defa367a89fc14eeeaff437
swpease <swpease@gmail.com>
2020-04-20 11:53:15 -0700

    [find and replace] find fwd implemented

--- OHTE/plaintext_find_replace_dialog.py
+++ OHTE/plaintext_find_replace_dialog.py
@@ -1,8 +1,9 @@
 import sys
+from typing import List
 
-from PySide2.QtWidgets import (QDialog, QLabel, QLineEdit, QDialogButtonBox, QPushButton,
+from PySide2.QtWidgets import (QDialog, QLabel, QLineEdit, QDialogButtonBox, QPushButton, QPlainTextEdit, QTextEdit,
                                QVBoxLayout, QHBoxLayout, QGridLayout, QCheckBox, QApplication)
-from PySide2.QtGui import QTextDocument
+from PySide2.QtGui import QTextDocument, QTextCursor, QColor
 from PySide2.QtCore import Qt
 
 
@@ -15,18 +16,25 @@ class PlainTextFindReplaceDialog(QDialog):
     Find wraps.
     Highlights all matches, operating on the closest-to-user's-cursor selection first,
     in the user-selected direction (Next / Previous).
+    Highlighting / found cursors retained on navigation back to text editor, and cleared on re-find/replace if
+    user modified the document.
     """
 
-    def __init__(self, document: QTextDocument, parent=None):
+    def __init__(self, plain_text_edit: QPlainTextEdit, parent=None):
         """
         Sets up the dialog.
 
-        :param document: Document to operate on.
+        :param plain_text_edit: Text Editor to operate on.
         :param parent: QWidget parent
         """
         super().__init__(parent=parent, f=Qt.Tool)
-        self.document = document
+        self.plain_text_edit = plain_text_edit
+        self.cursors_needed = True
+        self.find_flags = QTextDocument.FindFlags()
+        self.found_cursors: List[QTextCursor] = []
+        self.current_cursor = QTextCursor()
 
+        # UI
         layout = QVBoxLayout()
         find_and_replace_layout = QGridLayout()
         layout.addLayout(find_and_replace_layout)  # if QGL is sub-layout, add to parent layout before doing stuff.
@@ -70,17 +78,102 @@ class PlainTextFindReplaceDialog(QDialog):
 
         layout.addWidget(self.btn_box)
         self.setLayout(layout)
+        # End UI
 
         self.btn_box.rejected.connect(self.reject)
         self.find_line_edit.textEdited.connect(self._handle_text_edited)
+        self.find_next_btn.clicked.connect(self.next)
+        self.plain_text_edit.document().contentsChanged.connect(self.set_cursors_needed_true)
+        self.whole_word_check_box.stateChanged.connect(self.toggle_whole_word_flag)
+        self.match_case_check_box.stateChanged.connect(self.toggle_match_case_flag)
+
+    def closeEvent(self, arg__1):
+        self.plain_text_edit.setExtraSelections([])
+        super().closeEvent(arg__1)
+
+    def set_cursors_needed_true(self):
+        self.cursors_needed = True
+
+    def toggle_match_case_flag(self, state: int):
+        self.cursors_needed = True
+
+        if state == Qt.Unchecked:
+            self.find_flags &= ~QTextDocument.FindCaseSensitively
+        elif state == Qt.Checked:
+            self.find_flags |= QTextDocument.FindCaseSensitively
+
+    def toggle_whole_word_flag(self, state: int):
+        self.cursors_needed = True
+
+        if state == Qt.Unchecked:
+            self.find_flags &= ~QTextDocument.FindWholeWords
+        elif state == Qt.Checked:
+            self.find_flags |= QTextDocument.FindWholeWords
+
+    def find_all(self, text: str, document: QTextDocument, flags=QTextDocument.FindFlags()) -> List[QTextCursor]:
+        """
+        Finds all occurrences of `text` in `document`, in order.
+
+        :param text: Text to find.
+        :param document: Document to search.
+        :param flags: Conditions to set on the search: none or (whole word and/or match case)
+        :return: Ordered list of all found instances.
+        """
+        cursor = QTextCursor(document)  # default pos == 0
+        found: List[QTextCursor] = []
+
+        while True:
+            cursor = document.find(text, cursor, flags)
+            if cursor.isNull():
+                return found
+            else:
+                found.append(cursor)
+
+    def next(self):
+        if self.cursors_needed:
+            self.found_cursors = self.find_all(self.find_line_edit.text(), self.plain_text_edit.document(), self.find_flags)
+            self.cursors_needed = False
+            self.current_cursor = self.plain_text_edit.textCursor()  # returns copy of
+            self.plain_text_edit.setExtraSelections([])
+
+        if not self.found_cursors:
+            return
+
+        if self.current_cursor >= self.found_cursors[-1]:  # loop back to start. cursor equality based on position.
+            self.current_cursor = self.found_cursors[0]
+        else:
+            for cur in self.found_cursors:
+                if cur > self.current_cursor:  # next in order
+                    self.current_cursor = cur
+                    break
+        # move along text editor's viewport
+        next_pte_cursor = QTextCursor(self.current_cursor)
+        next_pte_cursor.clearSelection()
+        self.plain_text_edit.setTextCursor(next_pte_cursor)
+
+        #highlighting
+        normal_color = QColor(Qt.yellow).lighter()
+        current_color = QColor(Qt.magenta).lighter()
+        extra_selections: List[QTextEdit.ExtraSelection] = []
+        for cur in self.found_cursors:
+            selection = QTextEdit.ExtraSelection()
+            selection.cursor = cur
+            if cur == self.current_cursor:
+                selection.format.setBackground(current_color)
+            else:
+                selection.format.setBackground(normal_color)
+            extra_selections.append(selection)
+        self.plain_text_edit.setExtraSelections(extra_selections)
 
     def _handle_text_edited(self, text):
         """
-        Modifies button states.
+        Modifies button states and sets self.cursors_needed to True.
 
         :param text: The find_line_edit's text.
         :return: Side effect: btn enabled / default
         """
+        self.cursors_needed = True
+
         find_enabled = text != ""
         self.find_next_btn.setEnabled(find_enabled)
         self.find_prev_btn.setEnabled(find_enabled)
@@ -93,6 +186,8 @@ class PlainTextFindReplaceDialog(QDialog):
 
 if __name__ == '__main__':
     app = QApplication([])
-    d = PlainTextFindReplaceDialog(QTextDocument())
+    g = QPlainTextEdit()
+    d = PlainTextFindReplaceDialog(g)
+    g.show()
     d.show()
     sys.exit(app.exec_())
--- tests/pytest_tests/plaintext_find_replace_dialog_test.py
+++ tests/pytest_tests/plaintext_find_replace_dialog_test.py
@@ -1,19 +1,26 @@
+import pytest
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QTextDocument
-from PySide2.QtWidgets import QPushButton, QDialogButtonBox
+from PySide2.QtWidgets import QPushButton, QDialogButtonBox, QPlainTextEdit
 
 from OHTE.plaintext_find_replace_dialog import PlainTextFindReplaceDialog
+
+@pytest.fixture()
+def stuff():
+    g = QPlainTextEdit()
+    d = PlainTextFindReplaceDialog(g)
+    return g, d
 
 
 class TestInit(object):
     def test_window_flags(self, qtbot):
-        dialog = PlainTextFindReplaceDialog(QTextDocument())
+        dialog = PlainTextFindReplaceDialog(QPlainTextEdit())
         dialog.show()
         qtbot.addWidget(dialog)
         assert bool(dialog.windowFlags() & Qt.Tool)
 
     def test_btns_props(self, qtbot):
-        dialog = PlainTextFindReplaceDialog(QTextDocument())
+        dialog = PlainTextFindReplaceDialog(QPlainTextEdit())
         dialog.show()
         qtbot.addWidget(dialog)
         for btn in dialog.btn_box.buttons(): #type: QPushButton
@@ -25,7 +32,7 @@ class TestInit(object):
                 assert not btn.isDefault()
 
     def test_close_on_enter(self, qtbot):
-        dialog = PlainTextFindReplaceDialog(QTextDocument())
+        dialog = PlainTextFindReplaceDialog(QPlainTextEdit())
         dialog.show()
         qtbot.addWidget(dialog)
         qtbot.keyClick(dialog, Qt.Key_Return)
@@ -34,7 +41,7 @@ class TestInit(object):
 
 class TestBtnStates(object):
     def test_text_affects_state(self, qtbot):
-        dialog = PlainTextFindReplaceDialog(QTextDocument())
+        dialog = PlainTextFindReplaceDialog(QPlainTextEdit())
         dialog.show()
         qtbot.addWidget(dialog)
         qtbot.keyClick(dialog.find_line_edit, Qt.Key_R)
@@ -54,3 +61,127 @@ class TestBtnStates(object):
             else:
                 assert not btn.isEnabled()
                 assert not btn.isDefault()
+
+
+class TestNext(object):
+    def test_highlight_basic(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.keyClick(dc, Qt.Key_Return)  # not working...
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+
+        es = te.extraSelections()
+        assert len(es) == 3
+        # wraps around to first (b/c te cursor is at end of doc)
+        assert es[0].format.background().color().getRgb() != es[1].format.background().color().getRgb()
+        assert es[2].format.background().color().getRgb() == es[1].format.background().color().getRgb()
+
+    def test_cycles_fwd(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.keyClick(dc, Qt.Key_Return)  # not working...
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 3
+        assert es[0].format.background().color().getRgb() != es[1].format.background().color().getRgb()
+        assert es[2].format.background().color().getRgb() == es[0].format.background().color().getRgb()
+
+    def test_refinds_on_te_edit(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.keyClick(dc, Qt.Key_Return)  # not working...
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        qtbot.keyClick(te, Qt.Key_Backspace)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 2
+
+    def test_moves_te_cursor(self, stuff, qtbot):
+        # doing this updates the viewport to have current cursor selection in frame.
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.keyClick(dc, Qt.Key_Return)  # not working...
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        assert te.textCursor().position() == d.current_cursor.position()
+
+
+class TestClose(object):
+    def test_clears_highlights_on_close(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.keyClick(dc, Qt.Key_Return)  # not working...
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        d.close()
+        assert not te.extraSelections()
+
+
+class TestFlags(object):
+    def test_match_case_flag(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi hi Hi")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        # qtbot.mouseClick(d.match_case_check_box, Qt.LeftButton)  # not working...
+        d.match_case_check_box.setChecked(True)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 2
+
+        d.match_case_check_box.setChecked(False)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 3
+
+    def test_whole_word_flag(self, stuff, qtbot):
+        te: QPlainTextEdit = stuff[0]
+        d: PlainTextFindReplaceDialog = stuff[1]
+        qtbot.addWidget(te)
+        qtbot.addWidget(d)
+
+        te.setPlainText("hi Hi hii")
+        qtbot.keyClicks(d.find_line_edit, "hi")
+        d.whole_word_check_box.setChecked(True)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 2
+
+        d.whole_word_check_box.setChecked(False)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 3
+
+        # both together
+        d.whole_word_check_box.setChecked(True)
+        d.match_case_check_box.setChecked(True)
+        qtbot.mouseClick(d.find_next_btn, Qt.LeftButton)
+        es = te.extraSelections()
+        assert len(es) == 1
